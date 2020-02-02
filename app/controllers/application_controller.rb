1001d4e99b18d45ed1ca8141fa4512b097dc4d57
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-02 02:35:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       
       @team = Team.new(params[:team])
       params[:team][:members].each do |info|
-        Hero.new(params[])
+        Hero.new(params[:name], params[:power], params[:bio])
       end
         
       @heroes = Hero.all
