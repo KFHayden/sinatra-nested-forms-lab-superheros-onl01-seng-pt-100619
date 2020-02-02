942a61feb6a77df4375c0fa3cd80122dad6d78c1
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-02 02:22:11 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,11 +1,18 @@
 class Hero
   
+  @@all = []
+  
   attr_reader :name, :power, :bio
   
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
 end
